e9107b3384c0daa4ab018a514250811b4f922e61
Andre Vitorio <andre@vitorio.net>
2023-12-28 03:51:30 -0300

    Refactor ContentGrid component

--- src/components/ContentGrid.tsx
+++ src/components/ContentGrid.tsx
@@ -27,7 +27,7 @@ const ContentGrid = ({
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:gap-x-6 lg:gap-x-8 gap-y-5 sm:gap-y-6 lg:gap-y-8 mb-8">
         {items.map((item, id) => (
           <Link key={item.slug} href={`/${collection}/${item.slug}`}>
-            <div className="cursor-pointer border project-card rounded-md md:w-full scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100 hover:shadow overflow-hidden">
+            <div className="cursor-pointer border rounded-md md:w-full scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu transition duration-100 motion-reduce:hover:scale-100 hover:shadow overflow-hidden">
               <div className="sm:mx-0">
                 <Image
                   src={item.coverImage || `/api/og?title=${item.title}`}
@@ -38,11 +38,6 @@ const ContentGrid = ({
                   sizes="(min-width: 768px) 347px, 192px"
                   priority={priority && id <= 2}
                 />
-                {collection === "projects" && (
-                  <h2 className="p-2 bg-opacity-80 bg-white text-center whitespace-nowrap font-bold text-3xl absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 shadow-lg rounded-lg">
-                    {item.title}
-                  </h2>
-                )}
               </div>
               <div className="p-4">
                 {Array.isArray(item?.tags)
